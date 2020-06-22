3183212b2f5d01865f43e834706ca2d7e5f185a5
XaviFibla <xavi.fibla@gmail.com>
2020-06-22 16:12:04 +0200

    Validate password length

--- Comprovador de contrasenyes/Comprovador de contrasenyes/Program.cs
+++ Comprovador de contrasenyes/Comprovador de contrasenyes/Program.cs
@@ -4,9 +4,17 @@ namespace Comprovador_de_contrasenyes
 {
     class Program
     {
+        
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {  
+            Console.WriteLine("Password: ");
+            var password = Console.ReadLine();
+            var lengthValidation = new LengthValidation();
+
+            if (lengthValidation.Validate(password))
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Longitud mínim de 8 caràcters");
         }
     }
 }
