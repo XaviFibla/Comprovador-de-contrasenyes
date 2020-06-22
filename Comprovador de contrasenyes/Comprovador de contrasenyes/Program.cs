2d853dcb52a089508184e0e643b9776687ca5297
XaviFibla <xavi.fibla@gmail.com>
2020-06-22 16:05:18 +0200

    Initial commit

---
+++ Comprovador de contrasenyes/Comprovador de contrasenyes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Comprovador_de_contrasenyes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
