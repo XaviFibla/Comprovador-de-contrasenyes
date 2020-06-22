79ab52be09f299cc8f6570edb6ee15b7ec0f2233
XaviFibla <xavi.fibla@gmail.com>
2020-06-22 16:24:23 +0200

    Validate password contains letters and numbers

---
+++ Comprovador de contrasenyes/Comprovador de contrasenyes/DigitsValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Comprovador_de_contrasenyes
+{
+    public class DigitsValidation
+    {
+        public DigitsValidation() { }
+
+        public bool ContainsOnlyDigits(string password) {
+            return !password.All(char.IsDigit);
+        }
+    }
+}
--- Comprovador de contrasenyes/Comprovador de contrasenyes/Program.cs
+++ Comprovador de contrasenyes/Comprovador de contrasenyes/Program.cs
@@ -4,17 +4,27 @@ namespace Comprovador_de_contrasenyes
 {
     class Program
     {
-        
+
         static void Main(string[] args)
-        {  
+        {
             Console.WriteLine("Password: ");
             var password = Console.ReadLine();
-            var lengthValidation = new LengthValidation();
 
-            if (lengthValidation.Validate(password))
-                Console.WriteLine("OK");
-            else
+            var lengthValidation = new LengthValidation();
+            var digitValidation = new DigitsValidation();
+
+            if (!lengthValidation.Validate(password))
+            {
                 Console.WriteLine("Longitud mínim de 8 caràcters");
+            }
+            if (digitValidation.ContainsOnlyDigits(password))
+            {                 
+                Console.WriteLine("La contrasenya ha de contenir lletres i dígits"); 
+            }
+            else
+            {
+                Console.WriteLine("OK");
+            }
         }
     }
 }
